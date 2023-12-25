cbeb83e1e5b577cb6e0cdb42c0e395c406b373e6
huzejun <huzejun1990>
2023-12-26 06:47:31 +0800

    golang开发框架-golang中间件编程gin框架 2023-12-26 06:47:04

---
+++ main.go
@@ -0,0 +1,25 @@
+// @Author huzejun 2023/12/26 6:18:00
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	v2 := r.Group("/v2")
+	v1 := r.Group("/v1")
+	v1.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	v2.POST("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
---
+++ routers/router.go
@@ -0,0 +1,11 @@
+// @Author huzejun 2023/12/26 6:40:00
+package routers
+
+import "github.com/gin-gonic/gin"
+
+func InitRouters(r *gin.Engine) {
+	api := r.Group("/api")
+	//课程相关接口
+	//用户相关接口
+	//登陆
+}
